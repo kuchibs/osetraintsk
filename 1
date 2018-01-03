ce16e9d0245c16d934eeed23df3117f71e13a757
Sarvani K <kuchibs@gmail.com>
2018-01-03 04:14:11 -0500

    need to loop through file and take params per line - not working yet

---
+++ 1
@@ -0,0 +1,4 @@
+while read file key log 
+do
+  echo "cmd:" $cmd "key:"$key "log:"$log  "file:"$file
+done <  server.txt 
--- copyPubkey.sh
+++ copyPubkey.sh
@@ -1,11 +1,30 @@
 #!/bin/sh -x
-while read SERVER PASSWORD
+: <<'END'
+while read p; do
+  echo $p
+done <peptides.txt
+
+
+for word in $sentence
 do
-   sshpass -p "$PASSWORD" ssh root@"$SERVER" "mkdir -m 700 /root/.ssh"
-   sshpass -p "$PASSWORD" scp ~/.ssh/id_rsa.pub "$SERVER":/root/.ssh/id_rsa_9023Control.pub
+    echo $word
+done
+END
+
+#while read file SERVER PASSWORD
+#while read file PASSWORD SERVER
+
+while read file PASSWORD SERVER XYZ
+do
    
-   sshpass -p "$PASSWORD" ssh root@"$SERVER" "cat /root/.ssh/id_rsa_9023Control.pub >> /root/.ssh/authorized_keys"
-   sshpass -p "$PASSWORD" ssh root@"$SERVER" "chmod 600 /root/.ssh/authorized_keys"
+   echo "p:"$PASSWORD "s"$SERVER "cmd:"$cmd "file:"$file
+   sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no  root@"$file" "mkdir -m 700 /root/.ssh"
+   sshpass -p "$PASSWORD" scp ~/.ssh/id_rsa.pub "$file":/root/.ssh/id_rsa_9023Control.pub
+   
+   sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no root@"$file" "cat /root/.ssh/id_rsa_9023Control.pub >> /root/.ssh/authorized_keys"
+   sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no root@"$file" "chmod 600 /root/.ssh/authorized_keys"
+
+done <./server.txt
 
    #chmod 644 filename>>filename
    #cat id_rsa.pub >> ~/.ssh/authorized_keys
@@ -15,4 +34,3 @@ do
 
   #sshpass -p "interOP@9159" scp file.txt "osetrain-i9159":/root/.ssh/id_rsa_9023Control.pub
   # scp .ssh/id_rsa.pub root@kucsa02-i9021:/root/.ssh/id_rsa_9023Control.pub
-done <./server.txt
